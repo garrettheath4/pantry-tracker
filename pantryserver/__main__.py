118c2e770ec0144acf9cfd231257a860e453d9b2
Garrett Heath Koller <garrettheath4@gmail.com>
2019-11-21 00:45:25 -0500

    Added initial files for Python module

---
+++ pantryserver/__main__.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+"""Starts a web server that provides a pantry tracking web app."""
+
+__author__ = "Garrett Heath Koller"
+__copyright__ = "Copyright 2019, Garrett Heath Koller"
+__license__ = "MIT"
+__version__ = "1.0.0"
+__maintainer__ = "Garrett Heath Koller"
+__email__ = "garrettheath4@gmail.com"
+__status__ = "Prototype"
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
