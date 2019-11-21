a34eb5b96a6f0e54f54ae7277d45a5cb1b64b27c
Garrett Koller <gkoller@catalist.us>
2019-11-21 13:08:42 -0500

    Adding basic inventory table React components

---
+++ webapp/src/InventoryTable.jsx
@@ -0,0 +1,22 @@
+import React from "react"
+import ItemRow from "./ItemRow.jsx"
+
+const InventoryTable = () => {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Item</th>
+          <th>Qty</th>
+        </tr>
+      </thead>
+      <tbody>
+        <ItemRow name="Apples" />
+        <ItemRow name="Bananas" />
+      </tbody>
+    </table>
+  )
+}
+export default InventoryTable
+
+// vim: set ts=2 sw=2 vts=2 sta sts=2 sr et ai:
---
+++ webapp/src/ItemRow.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+const ItemRow = ({ name }) => {
+  return (
+    <tr>
+      <td>{name}</td>
+      <td>5</td>
+    </tr>
+  )
+}
+export default ItemRow
+
+// vim: set ts=2 sw=2 vts=2 sta sts=2 sr et ai:
