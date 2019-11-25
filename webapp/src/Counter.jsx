ff03c8822e25c1bcf11c437bbd518c3c37463a6a
Garrett Koller <gkoller@catalist.us>
2019-11-25 10:21:31 -0500

    React webapp now fetches item counts from API when page loads

--- webapp/src/Counter.jsx
+++ webapp/src/Counter.jsx
@@ -1,13 +1,34 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 
 const Counter = ({ name }) => {
-  const [count, setCountState] = useState(5)
+  const [count, setCountState] = useState(0)
+
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetch(`/api?name=${name.toLowerCase()}`)
+      res
+        .text()
+        .then(res => setCountState(Number(res)))
+        .catch(err => console.log("Unable to fetch count for", name, ".", err))
+    }
+
+    // noinspection JSIgnoredPromiseFromCall
+    fetchData();
+  })
 
   const setCount = (newCount) => (
     () => {
       const nonNegCount = Math.max(newCount, 0)
       setCountState(nonNegCount)
       fetch(`/api?name=${name.toLowerCase()}&count=${nonNegCount}`)
+        .then(res => res.text())
+        .then(res => {
+          if (nonNegCount !== Number(res)) {
+            console.log("Warning: Got", res.text(), "from API but expected",
+              nonNegCount)
+          }
+        })
+        .catch(err => console.log("Unable to set item count through API", err))
     }
   )
 
