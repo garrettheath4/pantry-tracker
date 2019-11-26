764aef991ebab6f1f51582f002fbfb669be4f26d
Garrett Koller <gkoller@catalist.us>
2019-11-25 22:55:44 -0500

    GET / requests now automatically return index.html

--- pantryserver/api/statichandler.py
+++ pantryserver/api/statichandler.py
@@ -1,25 +1,27 @@
-from os import curdir, sep
 import re
+from os import curdir, sep
+import logging
 
 from .basehandler import BaseRequestHandler
 
 
 class StaticFileRequestHandler(BaseRequestHandler):
 
+    static_folder = 'webapp' + sep + 'build'
+    content_types = {
+        '.html': 'text/html',
+        '.htm': 'text/html',
+        '.ico': 'image/x-icon',
+        '.png': 'image/png',
+        '.svg': 'image/svg+xml',
+        '.css': 'text/css',
+        '.js': 'application/javascript',
+        '.json': 'application/json',
+        '.map': 'application/x-navimap',
+    }
+
     # noinspection PyPep8Naming
     def do_GET(self):
-        content_types = {
-            '.html': 'text/html',
-            '.htm':  'text/html',
-            '.ico':  'image/x-icon',
-            '.png':  'image/png',
-            '.svg':  'image/svg+xml',
-            '.css':  'text/css',
-            '.js':   'application/javascript',
-            '.json': 'application/json',
-            '.map':  'application/x-navimap',
-        }
-        static_folder = 'webapp' + sep + 'build'
         file_extension_search = re.search('\\.[a-zA-Z]+$',
                                           self.path.split('?')[0])
 
@@ -29,17 +31,25 @@ class StaticFileRequestHandler(BaseRequestHandler):
             self._send_illegal_path_part('/.')
         elif '~' in self.path:
             self._send_illegal_path_part('~')
-        elif file_extension_search \
-                and file_extension_search.group(0) in content_types:
-            self._set_headers(content_types[file_extension_search.group(0)])
+        elif (file_extension_search and file_extension_search.group(0)
+                in StaticFileRequestHandler.content_types) or self.path == '/':
+            if self.path == '/':
+                file_path = 'index.html'
+                content_type = 'text/html'
+            else:
+                file_path = self.path
+                content_type = StaticFileRequestHandler.content_types[
+                    file_extension_search.group(0)]
             try:
-                f = open(curdir + sep + static_folder + sep + self.path, 'rb')
+                f = open(curdir + sep + StaticFileRequestHandler.static_folder
+                         + sep + file_path, 'rb')
+                self._set_headers(content_type)
                 self.wfile.write(f.read())
                 f.close()
             except IOError:
                 self._send_not_found()
         elif '.' in self.path:
-            print("Path does not have an approved extension - %s" % self.path)
+            logging.warning("Path does not have an approved extension - %s", self.path)
             self._send_not_found()
         else:
             self._send_not_found()
