e7ac0cf7aeba81b2b8c07dbf74ab35a0a95d6404
Garrett Heath Koller <garrettheath4@gmail.com>
2019-12-03 10:54:22 -0500

    Added git pull and npm build to start.sh script

--- start.sh
+++ start.sh
@@ -3,6 +3,11 @@
 # Switch to this script's directory (in case it's called from a different working directory)
 cd "$( dirname "${BASH_SOURCE[0]}" )" || exit
 
+git pull
+
 # Optional: pipenv install
+
+# Optional: npm run build
+cd webapp && npm run build && cd ..
 
 pipenv run python3 -m pantryserver
