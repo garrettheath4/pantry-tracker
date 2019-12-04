2d088803b2aa29b57189d18df0928c300cce8010
Garrett Koller <gkoller@catalist.us>
2019-12-04 15:46:47 -0500

    pantry-tracker-13 Singularize items with only 1 quantity

--- webapp/src/ItemCount.jsx
+++ webapp/src/ItemCount.jsx
@@ -5,7 +5,7 @@ import { roundToOne } from "./numbers"
 
 const ItemCount = ({ count, name }) => {
   return <span className="pure-u-3-4 countNumber">
-    {displayCount(count)} {name}
+    {displayCount(count)} {displayName(name, count)}
   </span>
 }
 export default ItemCount
@@ -15,6 +15,13 @@ const displayCount = (count) => (
     ? <FontAwesomeIcon icon="spinner" pulse />
     : roundToOne(count)
 )
+
+const displayName = (name, count) => {
+  if (count === 1 && name.endsWith("s")) {
+    return name.slice(0, -1)
+  }
+  return name
+}
 
 
 // vim: set ts=2 sw=2 vts=2 sta sts=2 sr et ai:
