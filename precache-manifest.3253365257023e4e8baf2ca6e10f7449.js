41e2aeac192a19ea281bfd526650ee389a5d77b4
Garrett Heath Koller <garrettheath4@gmail.com>
2020-02-04 10:16:05 +0000

    Updates

---
+++ precache-manifest.3253365257023e4e8baf2ca6e10f7449.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33e1834d3337ee7d60b565bd821ae30f",
+    "url": "/pantry-tracker/index.html"
+  },
+  {
+    "revision": "c7d8fc823ff20eccbe9b",
+    "url": "/pantry-tracker/static/css/main.256f8f25.chunk.css"
+  },
+  {
+    "revision": "b61827e6debbb8fb2bfa",
+    "url": "/pantry-tracker/static/js/2.842c22dc.chunk.js"
+  },
+  {
+    "revision": "8a14dcea7ad8b593c1e83c11f7ba1a89",
+    "url": "/pantry-tracker/static/js/2.842c22dc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c7d8fc823ff20eccbe9b",
+    "url": "/pantry-tracker/static/js/main.b59d7f8a.chunk.js"
+  },
+  {
+    "revision": "3ba3108517a5999e84f7",
+    "url": "/pantry-tracker/static/js/runtime-main.1cbe7af4.js"
+  }
+]);
